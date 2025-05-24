33dc4a573011dbac703e8ab615fa3fa861903530
kimdy <kimdy108@vizufon.co.kr>
2025-05-24 23:05:59 +0900

    JSP join and session

--- BBS/src/main/java/user/UserDAO.java
+++ BBS/src/main/java/user/UserDAO.java
@@ -13,7 +13,7 @@ public class UserDAO {
 	
 	public UserDAO() {
 		try {
-			String dbUrl = "jdbc:mysql://localhost:3308/BBS";
+			 String dbUrl = "jdbc:mysql://localhost:3308/BBS";
 			String dbID = "root";
 			String dbPassword = "ehduq108";
 			Class.forName("com.mysql.jdbc.Driver");
@@ -37,11 +37,27 @@ public class UserDAO {
 					return 0; // 비밀번호 불일치
 				}
 			}
-			return -1; // 아이디 없
+			return -1; // 아이디 없음 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return -2; // 데이터 베이스 오
+		return -2; // 데이터 베이스 오류
+	}
+	
+	public int join (User user) {
+		String SQL = "INSERT INTO USER VALUES (?, ?, ?, ?, ?)";
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, user.getUserID());
+			pstmt.setString(2, user.getUserPassword());
+			pstmt.setString(3, user.getUserName());
+			pstmt.setString(4, user.getUserGender());
+			pstmt.setString(5, user.getUserEmail());
+			return pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -1; // 데이터 베이스 오류
 	}
 
 }
